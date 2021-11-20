43d4ef30a76ad0f0a9d86137897b188f97fc1c9e
Viniciu Cristian <git.viniciu@gmail.com>
2021-11-20 19:08:06 -0300

    update delete rquest

--- src/app/pages/todo/todo.page.ts
+++ src/app/pages/todo/todo.page.ts
@@ -7,30 +7,55 @@ import { api } from 'src/services/api';
 })
 export class TodoPage implements OnInit {
   allTodo: any = [];
-  len: any;
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    this.confirmData();
+  }
+
+  confirmData() {
+    const IsHaveTodo = this.allTodo == null || 0 ? true : false;
+    if (!IsHaveTodo) {
+      this.getAll();
+    }
+  }
+
+  doRefresh(event) {
+    this.allTodo = []
     this.getAll();
 
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
   }
 
-  getAll(){
-    api.get('text/list').then(response => {
-      response.data.forEach(element => {
-      this.allTodo.push({
-        id: element.id,
-        text: element.text,
-      })
+  getAll() {
+    try {
+      api.get('text/list').then((response) => {
+        response.data.forEach((element) => {
+          this.allTodo.push({
+            id: element.id,
+            text: element.text,
+          });
+        });
       });
-    })
 
-    console.log(this.allTodo)
-    console.log(this.allTodo.length)
+      console.log(this.allTodo);
+    } catch (error) {
+      console.log(error);
+    }
   }
 
-  delete(id: string) {
+  deleteTodo(id: string) {
+    try {
 
+      api.delete(`/text/${id}`).then((response) => {
+        console.log(response.data);
+      });
+
+    } catch (error) {
+      console.log(error)
+    }
   }
-
 }
